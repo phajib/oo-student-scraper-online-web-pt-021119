c4481f430dd4cfd48c9b0fc6a9517017028b4500
Haj B <phajib@gmail.com>
2019-04-02 18:37:56 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,42 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html_link = Nokogiri::HTML(open(index_url))
+    students = []
+
+    #  Iterate through the card/s
+    html_link.css("div.roster-cards-container").each do |student_card|
+      student_card.css(".student-card a").each do |student|
+        # binding.pry
+        profile = "#{student.attr('href')}"
+        location = student.css('.student-location').text
+        name = student.css('.student-name').text
+        # student hash details inside students array
+        students << {name: name, location: location, profile_url: profile}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html_link = Nokogiri::HTML(open(profile_url))
+    student = {}
+
+    card = html_link.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
+      card.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        elsif link.include?(".com")
+          student[:blog] = link
+        end
+      end
+    student[:profile_quote] = html_link.css(".profile-quote").text
+    student[:bio] = html_link.css("div.description-holder p").text
+    student
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +7,32 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |attributes, value|
+      self.send("#{attributes}=", value)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+    # binding.pry
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attributes, value|
+      # binding.pry
+      # Iterate through the attributes hash and get each hash's value
+      self.send("#{attributes}=", value)
+    end
+    # binding.pry
+    # return all of the values from the all array
+    @@all
   end
 
   def self.all
-    
+    @@all
   end
 end
 
